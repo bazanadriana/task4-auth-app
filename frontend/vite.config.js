8bff1dad4cc0c2926a66ab00bcb446eac9dafea9
Adriana Bazan <adrianabazan@Adrianas-MacBook-Pro.local>
2025-08-02 17:24:07 -0600

    Build frontend and fix Vite production config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
   server: {
     proxy: {
-      '/api': 'http://localhost:5000'
-    }
+      '/api': 'http://localhost:5000',
+    },
   },
-  build: {
-    outDir: 'dist'
-  }
 });
+
